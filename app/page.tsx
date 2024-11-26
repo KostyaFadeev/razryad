3b9902f6d1a45ea99519ea5c229de5a5fabc6b0f
Konstantin Fadeev <kostya.fad1@gmail.com>
2024-11-27 01:25:00 +0500

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon, TelegramIcon } from "@/components/icons";
-import { AboutBlock } from "@/components/AboutBlock";
+import { TelegramIcon } from "@/components/icons";
 
 export default function Home() {
   return (
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -84,7 +84,7 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-labe l="Github" href={siteConfig.links.github}>
+        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
           <GithubIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
